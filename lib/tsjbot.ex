b099f52a824906144c9588670777fc22ffc9e882
Alexey Bolshakov <ua3mqj@example.com>
2017-12-12 22:15:37 +0000

    initial 2

---
+++ lib/tsjbot.ex
@@ -0,0 +1,18 @@
+defmodule TSJBot do
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: HttpTest2.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+end
---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule TSJBot.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :tsjbot,
+     version: "2017.12.13",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger],
+     mod: {TSJBot, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:distillery, "~> 1.5"},
+    ]
+  end
+end
