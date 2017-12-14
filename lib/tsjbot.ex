a8a6b1aa44c02f50efc45ca20e53e0226222f8a1
AlexB <ua3mqj@gmail.com>
2017-12-14 15:40:29 +0300

    remove trash code

--- lib/tsjbot.ex
+++ lib/tsjbot.ex
@@ -5,13 +5,10 @@ defmodule TSJBot do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
+    opts = [strategy: :one_for_one]
     children = [
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: HttpTest2.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
